bb677068d91cee69dedf8b877daa8a697ff0b791
dave oh <hirakoshinji722@gmail.com>
2021-02-28 13:31:24 +0100

    add git, use kotlin DSL for dependencies and add ben manes update gradle plugin

---
+++ buildSrc/src/main/java/io/davidosemwota/puppy/buildsource/BuildPlugins.kt
@@ -0,0 +1,8 @@
+@file:Suppress("SpellCheckingInspection")
+package io.davidosemwota.puppy.buildsource
+
+object BuildPlugins {
+
+    const val benManes =  "com.github.ben-manes.versions"
+    const val spotless = "com.diffplug.spotless"
+}
