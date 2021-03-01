1f17a44e68d0ae6979b76ec8f5b586c62d6c2b4a
dave oh <hirakoshinji722@gmail.com>
2021-03-01 13:48:52 +0100

    add MainViewModel.kt and PuppyItem composable

--- app/src/main/java/io/davidosemwota/puppy/ui/main/Main.kt
+++ app/src/main/java/io/davidosemwota/puppy/ui/main/Main.kt
@@ -8,17 +8,25 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import dev.chrisbanes.accompanist.coil.CoilImage
 import io.davidosemwota.puppy.R
+import io.davidosemwota.puppy.data.Pup
 import io.davidosemwota.puppy.ui.theme.PuppyTheme
+import androidx.compose.runtime.getValue
 
 @Composable
 fun Main() {
+    val viewModel = viewModel(MainViewModel::class.java)
+    val viewState by viewModel.state.collectAsState()
+
     Surface(Modifier.fillMaxSize()) {
         MainScreen(
 
@@ -73,6 +81,31 @@ fun MainScreen(
         )
     }
 
+}
+
+@Composable
+fun PuppyItem(
+    pup: Pup
+) {
+    Column {
+        CoilImage(
+            data = pup.imageUrl,
+            contentDescription = stringResource(R.string.puppy_item_image_desc),
+            loading = {
+
+            },
+            error = {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_broken_image),
+                    contentDescription = stringResource(R.string.broken_image_desc)
+                )
+            }
+        )
+        Text(
+            text = pup.name,
+            style = MaterialTheme.typography.h3
+        )
+    }
 }
 
 @Composable
---
+++ app/src/main/java/io/davidosemwota/puppy/ui/main/MainViewModel.kt
@@ -0,0 +1,25 @@
+package io.davidosemwota.puppy.ui.main
+
+import androidx.lifecycle.ViewModel
+import io.davidosemwota.puppy.data.DataSource
+import io.davidosemwota.puppy.data.Pup
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+
+class MainViewModel(
+    private val dataSource: DataSource = DataSource
+) : ViewModel() {
+
+    private val _state = MutableStateFlow(MainViewState())
+    val state: StateFlow<MainViewState>
+        get() = _state
+
+    init {
+        _state.value = MainViewState(dataSource.getAllPups())
+    }
+}
+
+data class MainViewState(
+    val puppies: List<Pup> = emptyList(),
+    val errorMessage: String? = null
+)
